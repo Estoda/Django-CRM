e26ab7c91339251cb899cdd02b6041e0491367bf
Ahmed Amin <programmer82253@gmail.com>
2025-01-17 05:04:01 +0200

    registeration

--- mydb.py
+++ mydb.py
@@ -1,22 +1,30 @@
-# Install Mysql on your computer
-# https://dev.mysql.com/downloads/installer/
-# pip install mysql
-# pip install mysql-connector
-# pip install mysql-connector-python
+# # Install Mysql on your computer
+# # https://dev.mysql.com/downloads/installer/
+# # pip install mysql
+# # pip install mysql-connector
+# # pip install mysql-connector-python
 
 
-import mysql.connector
+# import mysql.connector
 
-dataBase = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = 'AhmedAmin@82253',
-)
+# dataBase = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="AhmedAmin@82253",
+# )
 
-# Prepare a cursor object
-cursorObject = dataBase.cursor()
+# # Prepare a cursor object
+# cursorObject = dataBase.cursor()
 
-# Create a database
-cursorObject.execute("CREATE DATABASE CRM")
+# # Create a database
+# cursorObject.execute("CREATE DATABASE CRM")
 
-print("All Done!")
+# print("All Done!")
+
+
+def name_shuffler(str_):
+    s = str_.find(" ")
+    return str_[s + 1 :] + " " + str_[0:s]
+
+
+print(name_shuffler("Ahmed Amin"))
